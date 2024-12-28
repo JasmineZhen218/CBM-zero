54a7baa1e201ae468be8abfbf4e870a664d1a28d
JasmineZhen218 <zwang218@jhu.edu>
2024-12-27 21:38:03 -0500

    add comments

--- evaluation_utils.py
+++ evaluation_utils.py
@@ -2,32 +2,40 @@ import numpy as np
 import pandas as pd
 import torch
 def calculate_x_factuality(
-    data_name, classes, concepts_bank, ground_truth, feature_importance, top_k=10
+   data_name, classes, concepts_bank, ground_truth, feature_importance, top_k=10
 ):
-    # top-weighted positive concept ratio
+    """
+    Calculate the factuality of top-weighted positive concepts for given classes.
+    Parameters:
+        data_name (str): The name of the dataset.
+        classes (list): A list of class names.
+        concepts_bank (list): A list of concepts.
+        ground_truth (pd.DataFrame): A DataFrame containing ground truth data with columns 'class', 'concept', and 'weight'.
+        feature_importance (numpy.array): normalized global concept weights: (num_classes, num_concepts)
+        top_k (int, optional): The number of top concepts to consider. Default is 10.
+    Returns:
+        tuple: A tuple containing the mean and standard deviation of the top-weighted positive concept ratios.
+    """
+    # calculate the ratio of top-weighted reasonable concepts
     Ratios = []
     for i, cls in enumerate(classes):
-            if cls not in ground_truth["class"].values:
-                continue
-            if len(ground_truth.loc[ground_truth["class"] == cls, 'concept'].unique()) < 5:
-                continue
-            weights = feature_importance[i]
-            top_concepts_idx = np.argsort(weights)[::-1][:top_k]
-            top_concepts = np.array(concepts_bank)[top_concepts_idx]
-            mask = (ground_truth["class"] == cls) * (ground_truth["concept"].isin(top_concepts))
-            df_m = ground_truth[mask]
-            ratios = []
-            if data_name == "cub":
-                ratios.append(
+        if cls not in ground_truth["class"].values:
+            # skip classes not in ground truth
+            continue
+        if len(ground_truth.loc[ground_truth["class"] == cls, 'concept'].unique()) < 10:
+            # skip classes with less than 10 concepts
+            continue
+        weights = feature_importance[i]
+        top_concepts_idx = np.argsort(weights)[::-1][:top_k]
+        top_concepts = np.array(concepts_bank)[top_concepts_idx]
+        mask = (ground_truth["class"] == cls) * (ground_truth["concept"].isin(top_concepts))
+        df_m = ground_truth[mask]
+        if data_name == "cub":
+            Ratios.append(
                     len(df_m.loc[df_m["weight"] >= 50, "concept"].unique()) / top_k
                 )
-                Ratios.append(ratios)
-            else:
-                ratios.append(len(df_m.loc[df_m["weight"] > 0, "concept"].unique()) / top_k)
-                Ratios.append(ratios)
-    Ratios = np.array(Ratios)
-    mean = np.mean(Ratios, axis=0)
-    std = np.std(Ratios, axis=0)
-    print(f"top_k = {top_k} |")
-    print("\t Top weighted positive concept: {:.3f} +/- {:.3f}".format(mean[0], std[0]))
-    return mean[0], std[0]
+        else:
+            Ratios.append(len(df_m.loc[df_m["weight"] > 0, "concept"].unique()) / top_k)
+    mean = np.mean(Ratios)
+    std = np.std(Ratios)
+    return mean, std
